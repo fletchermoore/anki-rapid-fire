571f0d5b747b91181c3c08da5760ea4b69b9dacd
Fletcher Moore <fletcherbmoore@gmail.com>
2019-03-17 07:49:23 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# anki-rapid-fire
+# Anki Rapid Fire
+
+Add-on for Anki 2.1
+
+The goal of this project is the help you learn unordered flashcards faster.
+
+Imagine you have a deck of 500 vocabulary words. Some of them will be easier than
+others to memorize. Let's say your minimum interval is 1 minute. You will end up
+cycling through 1 minute worth of your hardest words as you learn the easy ones, leaving
+only hard words in your learning queue. To
+fix this problem, this addon moves "learning" cards to the bottom
+of the new card queue if you miss them 5 times in a row. That way you will blaze
+through all the easy cards and you won't be looking at hard cards for the entire
+study period.
+
+Currently, this also does not
+change the experimental scheduler. 
+
+**Warning: This will  reset the data on your _lapsed_ cards as well
+if you keep missing them, which you may not want.**
+
+## TODO
+
+- Make this a per-deck option
+- Make the number of misses required to trigger a reset customizable.
+- Add option to disable tooltip.
+
---
+++ src/rapid_fire/__init__.py
@@ -0,0 +1,42 @@
+# import the main window object (mw) from aqt
+from aqt import mw
+# import the "show info" tool from utils.py
+from aqt.utils import showText, tooltip
+
+import anki.sched as sched
+
+# takes a card revlog containing only [(ease,), ...]
+# returns number of times ease 1 was selected in a row
+# ie, number of consecutive times you said you didn't know the answer
+def missCount(log):
+    missCount = 0
+    index = len(log) - 1
+    while index > -1:
+        ease = log[index][0]
+        if ease == 1:
+            missCount += 1
+        else:
+            break
+        index = index - 1
+    return missCount
+
+
+
+oldAnswerCard = sched.Scheduler.answerCard
+
+def revisedAnswerCard(self, card, ease):
+    oldAnswerCard(self, card, ease)
+    entries = self.col.db.all(
+            "select ease from revlog where cid = ?", card.id)
+    misses = missCount(entries)
+    if misses > 2:
+        tooltip("To the back of the bus!")
+        self.forgetCards([card.id])
+        self.sortCards([card.id]) # set due to 1 (back of the bus!)
+        self.newCount += 1
+    #showText(str(entries) + " " + str(misses))
+
+sched.Scheduler.answerCard = revisedAnswerCard
+
+
+
